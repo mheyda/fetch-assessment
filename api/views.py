93afb4bc176a9584902b98f41fdfe2fade4fb54b
Marshall <marshallheyda@gmail.com>
2022-07-01 15:10:32 -0500

    Cleanup code

--- api/views.py
+++ api/views.py
@@ -4,30 +4,16 @@ from datetime import datetime
 import time
 from django.http import HttpResponse, HttpResponseRedirect
 import json
+from django.utils.safestring import mark_safe
 
-
-data = {
-    "transactions": [
-        { "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" },
-        { "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" },
-        { "payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z" },
-        { "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" },
-        { "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" },
-    ],
-    "balances": {
-        "DANNON": 1100, 
-        "UNILEVER": 200, 
-        "MILLER COORS": 10000
-    }
-}
-
-payers = {}
-
+transaction_data = []
+balance_data = {}
 
 # Forms
 class add_transaction(forms.Form):
-    payer = forms.CharField(label="Payer", widget=forms.TextInput())
-    points = forms.DecimalField(label="Number of Points", decimal_places = 0, max_digits = 6, widget=forms.TextInput())
+    payer = forms.CharField(label="Payer: ", widget=forms.TextInput())
+    points = forms.DecimalField(label="Number of Points: ", decimal_places = 0, max_digits = 6, widget=forms.TextInput())
+    timestamp = forms.CharField(label=mark_safe("Timestamp (optional, defaults to current time)<br />Format: yyyy-mm-ddThh:mm:ssZ<br />Example: 2020-11-02T14:00:00Z"), widget=forms.TextInput(), required=False)
 add_transaction_form = add_transaction()
 
 class spend_points(forms.Form):
@@ -35,100 +21,107 @@ class spend_points(forms.Form):
 spend_points_form = spend_points()
 
 
+# Home page
 def index(request):
     return render(request, "index.html", {
         "add_transaction_form": add_transaction_form,
         "spend_points_form": spend_points_form,
-        "transactions": data["transactions"],
-        "balances": data["balances"],
+        "transactions": transaction_data,
+        "balances": balance_data,
     })
 
-
+# Transactions route
 def transactions(request):
 
+    # User wants to post a transaction
     if request.method == "POST":
-        # Extract transaction inputs
+
+        # Extract transaction data from request
         payer = request.POST["payer"]
         points = int(request.POST["points"])
-
-        # Get date and time of transaction
-        timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        if request.POST["timestamp"]:
+            timestamp = request.POST["timestamp"]
+        else:
+            timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
 
         # Push transaction to storage and sort by timestamp
-        data["transactions"].append({"payer": payer, "points": points, "timestamp": timestamp})
-        data["transactions"].sort(key=lambda trans:time.mktime(time.strptime(trans['timestamp'], '%Y-%m-%dT%H:%M:%SZ')))
+        transaction_data.append({"payer": payer, "points": points, "timestamp": timestamp})
+        transaction_data.sort(key=lambda trans:time.mktime(time.strptime(trans['timestamp'], '%Y-%m-%dT%H:%M:%SZ')))
 
         # Push payer to balances if they don't already exist
-        if not payer in data["balances"]:
-            data["balances"][payer] = points
+        if not payer in balance_data:
+            balance_data[payer] = points
         # Otherwise adjust balances accordingly
         else:
-            data["balances"][payer] = data["balances"][payer] + points      
+            balance_data[payer] = balance_data[payer] + points      
 
     return HttpResponseRedirect("/")
 
 
+# Points route
 def points(request):
 
-    # Spend points
+    # User wants to spend points
     if request.method == "POST":
-        points_to_spend = int(request.POST["points"])
-        transaction_index = 0
-        points_spent = {}
-        original_balances = data["balances"].copy()
+        points_to_spend = int(request.POST["points"]) # Extract number of points to spend from request
+        transaction_index = 0 # Iterator to loop through transactions
+        original_balances = balance_data.copy() # Copy original balances to calculate output later
 
-        while points_to_spend > 0:
-            current_payer = data["transactions"][transaction_index]["payer"]
-
-            # Make sure the current payer's balance is large enough to handle current transaction
-            if data["balances"][current_payer] > data["transactions"][transaction_index]["points"]:
-                # If there's enough from the current transaction
-                if data["transactions"][transaction_index]["points"] > points_to_spend:
-                    if current_payer in points_spent:
-                        points_spent[current_payer] = points_spent[current_payer] - points_to_spend
-                    else:
-                        points_spent[current_payer] = -points_to_spend
-                    data["balances"][current_payer] = data["balances"][current_payer] - points_to_spend
-                    points_to_spend = 0
-
-                # If more transactions are needed
-                else:
-                    if current_payer in points_spent:
-                        points_spent[current_payer] = points_spent[current_payer] - data["transactions"][transaction_index]["points"]
-                    else:
-                        points_spent[current_payer] = -data["transactions"][transaction_index]["points"]
-                    data["balances"][current_payer] = data["balances"][current_payer] - data["transactions"][transaction_index]["points"]
-                    points_to_spend = points_to_spend - data["transactions"][transaction_index]["points"]
-                    
-            # If transaction is more than current payer's balance
-            else:
-                # If current payer has more points than need to be spent, finish
-                if data["balances"][current_payer] > points_to_spend:
-                    if current_payer in points_spent:
-                        points_spent[current_payer] = points_spent[current_payer] - points_to_spend
-                    else:
-                        points_spent[current_payer] = -points_to_spend
-                    data["balances"][current_payer] = data["balances"][current_payer] - points_to_spend
-                    points_to_spend = 0
-                # Empty current payer's balance and continue
-                else:                    
-                    if current_payer in points_spent:
-                        points_spent[current_payer] = points_spent[current_payer] - data["balances"][current_payer]
-                    else:
-                        points_spent[current_payer] = -data["balances"][current_payer]
-                    points_to_spend = points_to_spend - data["balances"][current_payer]
-                    data["balances"][current_payer] = 0
+        # Make sure the user has enough points
+        if sum(balance_data.values()) > points_to_spend:
             
-            transaction_index = transaction_index + 1
+            # Loop through transactions starting with the oldest
+            while points_to_spend > 0:
 
-        output = []
-        for key, value in data["balances"].items():
-            output.append({ "payer": key, "points": value - original_balances[key] })
+                # Save values for current comparison
+                current_payer = transaction_data[transaction_index]["payer"]
+                current_payer_balance = balance_data[current_payer]
+                current_transaction_points = transaction_data[transaction_index]["points"]
 
-        return HttpResponse(json.dumps(output))
+                # Check if current payer's balance is more than what the current transaction requires
+                if current_payer_balance > current_transaction_points:
+
+                    # If there's enough from the current transaction
+                    if current_transaction_points > points_to_spend:
+                        current_payer_balance = current_payer_balance - points_to_spend
+                        points_to_spend = 0
+
+                    # If more transactions are needed
+                    else:
+                        current_payer_balance = current_payer_balance - current_transaction_points
+                        points_to_spend = points_to_spend - current_transaction_points
+                        
+                # If transaction is more than current payer's balance
+                else:
+                    # If current payer has more points than need to be spent, finish
+                    if current_payer_balance > points_to_spend:
+                        current_payer_balance = current_payer_balance - points_to_spend
+                        points_to_spend = 0
+                    # Empty current payer's balance and continue
+                    else:                    
+                        points_to_spend = points_to_spend - current_payer_balance
+                        current_payer_balance = 0
+
+                # Update data that was passed by value and increment
+                balance_data[current_payer] = current_payer_balance
+                transaction_index = transaction_index + 1
+
+            # Generate output based on what was spent by who
+            output = []
+            for key, value in balance_data.items():
+                output.append({ "payer": key, "points": value - original_balances[key] })
+
+            return HttpResponse(json.dumps(output))
+
+        # User doesn't have enough points
+        else:
+            return HttpResponse("You don't have enough points!")
+
+        
 
 
-
+# Balances route
 def balances(request):
+    # User wants to view the balances from all payers
     if request.method == "GET":
-        return HttpResponse(json.dumps(data["balances"]))
+        return HttpResponse(json.dumps(balance_data))
